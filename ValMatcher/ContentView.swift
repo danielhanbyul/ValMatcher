5ca01de9620f8d2029fa9afb96c6683a93e9d216
danielhanbyul <150879381+danielhanbyul@users.noreply.github.com>
2024-06-06 20:50:14 -0700

    Initial Commit

---
+++ ValMatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ValMatcher
+//
+//  Created by Daniel Han on 6/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ValMatcher/ValMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValMatcherApp.swift
+//  ValMatcher
+//
+//  Created by Daniel Han on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ValMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
