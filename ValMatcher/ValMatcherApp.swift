eb130a5251cc394b2089e2b9476be02acb6e7ef0
danielhanbyul <150879381+danielhanbyul@users.noreply.github.com>
2024-07-18 17:51:15 -0700

    firebase stuff

--- ValMatcher/ValMatcherApp.swift
+++ ValMatcher/ValMatcherApp.swift
@@ -4,26 +4,19 @@
 //
 //  Created by Daniel Han on 6/6/24.
 //
-
-import SwiftUI
-import Firebase
-
 import SwiftUI
 import Firebase
 
 @main
 struct ValMatcherApp: App {
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() // Ensure this is your main view
         }
     }
 }
-
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
