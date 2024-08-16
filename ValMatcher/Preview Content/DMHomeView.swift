1b2dd4c2629603c041e9ba8d2d69e1922ccd9e18
danielhanbyul <150879381+danielhanbyul@users.noreply.github.com>
2024-08-16 12:45:37 +0900

    3

--- ValMatcher/Preview Content/DMHomeView.swift
+++ ValMatcher/Preview Content/DMHomeView.swift
@@ -322,10 +322,6 @@ struct DMHomeView: View {
         group.notify(queue: .main) {
             self.matches = updatedMatches
             self.totalUnreadMessages = count
-            // Notify the user when there are unread messages
-            if count > 0 {
-                showNotification(title: "New Messages", body: "You have \(count) unread messages.")
-            }
         }
     }
 
