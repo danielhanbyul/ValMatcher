1d685372c8ca45adc92e8bf16d0fb44115b0816c
danielhanbyul <150879381+danielhanbyul@users.noreply.github.com>
2024-10-08 00:07:48 +0900

    fixed kne beore

--- ValMatcher/Preview Content/DM.swift
+++ ValMatcher/Preview Content/DM.swift
@@ -11,7 +11,6 @@ import Firebase
 struct DM: View {
     var matchID: String
     var recipientName: String
-    @State private var isInChatView = false // State to track if user is in ChatView
 
     init(matchID: String, recipientName: String) {
         self.matchID = matchID
@@ -23,14 +22,7 @@ struct DM: View {
             LinearGradient(gradient: Gradient(colors: [Color(red: 0.02, green: 0.18, blue: 0.15), Color(red: 0.21, green: 0.29, blue: 0.40)]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
 
-            // Pass isInChatView state to ChatView
-            ChatView(matchID: matchID, recipientName: recipientName, isInChatView: $isInChatView)
-                .onAppear {
-                    isInChatView = true // Set isInChatView to true when this view appears
-                }
-                .onDisappear {
-                    isInChatView = false // Reset isInChatView to false when this view disappears
-                }
+            ChatView(matchID: matchID, recipientName: recipientName)
         }
     }
 }
