7d71a89d3754d54fae366fc0002ecae812734171
danielhanbyul <150879381+danielhanbyul@users.noreply.github.com>
2024-10-08 16:03:45 +0900

    ok so whatever i said in the previous commit, ig nore it and apply it to this one. (that one had a small error in dm)

--- ValMatcher/Preview Content/DM.swift
+++ ValMatcher/Preview Content/DM.swift
@@ -23,7 +23,7 @@ struct DM: View {
             LinearGradient(gradient: Gradient(colors: [Color(red: 0.02, green: 0.18, blue: 0.15), Color(red: 0.21, green: 0.29, blue: 0.40)]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
 
-            ChatView(matchID: matchID, recipientName: recipientName, isInChatView: $isInChatView)
+            ChatView(matchID: matchID, recipientName: recipientName)
         }
     }
 }
